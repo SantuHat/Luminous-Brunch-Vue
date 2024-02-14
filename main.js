43030edd4698d4d5b0228f7a928fbd8935f612ca
SHUO <iwilliam147258@gmail.com>
2024-02-14 11:22:06 +0800

    路由表新增

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ import './pages/6-1-1-2_reservation-info.js';
 import './pages/index.js';
 import './pages/4_menuDetail.js'
 import './pages/MealData.js'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router.js'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
